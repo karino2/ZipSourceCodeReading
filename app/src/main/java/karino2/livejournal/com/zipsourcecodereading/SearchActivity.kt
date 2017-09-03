00de319505ded1d13e0653ee91e355e84a947411
karino2 <hogeika2@gmailcom>
2017-09-04 00:48:14 +0900

    Remove temp debug codes and misc update
    - Remove useless toolbar margin and change background color
    - Remove debug GO button
    - Show hint and remove useless initial text
    - Show softkey when search start

--- app/src/main/java/karino2/livejournal/com/zipsourcecodereading/SearchActivity.kt
+++ app/src/main/java/karino2/livejournal/com/zipsourcecodereading/SearchActivity.kt
@@ -1,5 +1,6 @@
 package karino2.livejournal.com.zipsourcecodereading
 
+import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -7,6 +8,7 @@ import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.Toolbar
 import android.view.*
 import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.TextView
 import com.google.re2j.Pattern
@@ -39,10 +41,9 @@ class SearchActivity : AppCompatActivity() {
             holder.filePathTV.text = item.fentry
             holder.matchLineTV.text = item.line
 
-            // todo onitem click listener
             with(holder.view) {
                 tag = item
-                setOnClickListener { showMessage("clicked: " + holder.filePathTV.text.toString() ) }
+                setOnClickListener { openFile(holder.filePathTV.text.toString()) }
             }
         }
 
@@ -125,60 +126,27 @@ class SearchActivity : AppCompatActivity() {
                     ada.notifyDataSetChanged()
                 }
 
-        findViewById(R.id.goButton).setOnClickListener {
-
-            val cond = (findViewById(R.id.fileEntryField) as EditText).text.toString()
-
-            sourceArchive.listFiles()
-                    .filter{ it.name.contains(cond) }
-                    .take(1)
-                    .subscribeOn(Schedulers.io())
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe { ent -> openFile(ent) }
-
-        }
 
         (findViewById(R.id.searchEntryField) as EditText).setOnEditorActionListener(fun(view, actionId, keyEvent)  : Boolean {
             if(actionId == EditorInfo.IME_ACTION_SEARCH) {
+                hideSoftkey()
                 startSearch()
                 return true;
             }
             return false;
         })
 
-        /*
-        (findViewById(R.id.fileEntryField) as EditText).setOnEditorActionListener(fun (textView, actionId, keyEvent) : Boolean{
-            keyEvent?.let {
-                if(actionId == EditorInfo.IME_ACTION_DONE) {
-                    showMessage("deb: ")
-                    // let system handle ime related operation.
-                    return false
-                }
-            }
-            when(keyEvent?.action) {
-                KeyEvent.ACTION_DOWN -> {
-                    showMessage("deb: ")
-                    return true
-                }
-                null -> {
-                    if(actionId == EditorInfo.IM)
-                }
-            }
-            return false
-        })
-        */
     }
 
-    private fun  openFile(ent: ZipEntry) {
-        val intent = Intent(this, SourceViewActivity::class.java)
-        intent.putExtra("ZIP_FILE_ENTRY", ent.toString())
-        startActivity(intent)
-        /*
-        val reader = BufferedReader(InputStreamReader(sourceArchive.getInputStream(ent)), 8*1024)
+    fun hideSoftkey() {
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(currentFocus.windowToken, 0)
+    }
 
-        val lines = reader.readLines()
-        (findViewById(R.id.contentArea) as TextView).text = lines.joinToString("\n")
-        */
+    private fun  openFile(ent: String) {
+        val intent = Intent(this, SourceViewActivity::class.java)
+        intent.putExtra("ZIP_FILE_ENTRY", ent)
+        startActivity(intent)
     }
 
     fun showMessage(msg : String) = MainActivity.showMessage(this, msg)
